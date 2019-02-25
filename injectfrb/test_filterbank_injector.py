a0870dc80833da1ecd09104bf3befc96bc801703
Liam Connor <liam.dean.connor@gmail.com>
2019-02-25 14:04:06 +0100

    test script to see if filterbank reader works

--- injectfrb/test_filterbank_injector.py
+++ injectfrb/test_filterbank_injector.py
@@ -5,6 +5,8 @@ import inject_frb
 fn_fil = 'data/test.fil'
 fn_fil_out = 'data/'
 
+print("Reading in test.fil to verify filterbank reader/writer works")
+
 inject_frb.inject_in_filterbank(fn_fil, fn_fil_out, N_FRB=10,
                                 NTIME=2**15, rfi_clean=False,
                                 calc_snr=False, start=0,
