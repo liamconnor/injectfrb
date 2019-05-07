5d5fcd8907f98a1aa33a7b1655454d04f2d7924e
Liam Connor <liam.dean.connor@gmail.com>
2019-05-07 14:48:46 +0200

    now three ways to decide if FRB was detected

--- injectfrb/blind_detection.py
+++ injectfrb/blind_detection.py
@@ -7,6 +7,8 @@ DM and arrival times.
 """
 
 import numpy as np
+import matplotlib.pylab as plt
+from scipy import interpolate 
 
 import simulate_frb
 #import simpulse 
@@ -82,7 +84,7 @@ class DetectionDecision():
     """
 
     def __init__(self, dm, t0, width_i=0.001, 
-                 spec_ind=0., freq_ref=np.inf, scat_tau_ref=0.0,
+                 spec_ind=0., freq_ref=None, scat_tau_ref=0.0,
                  freq=(1550, 1250), nfreq=1536, fluence=1, dt=8.192e-5):
 
         self._dm = dm
@@ -90,6 +92,10 @@ class DetectionDecision():
         self._width_i = width_i
         self._spec_ind = spec_ind
         self._freq_ref = freq_ref
+
+        if freq_ref is None:
+            self._freq_ref = 0.5*(freq[0]+freq[-1])
+
         self._scat_tau_ref = scat_tau_ref
         self._freqs = np.linspace(freq[0], freq[-1], nfreq)
         self._bw = self._freqs.max() - self._freqs.min()
@@ -137,9 +143,112 @@ class DetectionDecision():
 
         return data_injfrb
 
+    def gen_dm_time_gaussian(self):
+        sigdm = 0.1*self._dm
+        sigt = 0.1*(1 + self._width_i/0.001) # scale for pulse width, min 0.1 sec 
+        ntime = 1000
+        ndm = 1000
 
-    def dm_time_box(self, dm_guess, t0_guess, 
-                    dm_err=0.1, t_err=0.5):
+        times = np.linspace(0, 100*self._dt*ntime, ntime)
+        times -= 0.5*ntime*self._dt*100
+        times += self._t0
+        dms = np.linspace(0.*self._dm, 2*self._dm, ndm)
+
+        dmtarr = np.exp(-0.5*(self._t0-times[None])**2/sigt**2-0.5*(self._dm-dms[:,None])**2/sigdm**2)
+        dmtarr_function = interpolate.interp2d(times, dms, dmtarr)
+
+        return dmtarr, dms, times, dmtarr_function
+
+    def gen_dm_time_bowtie(self, simulator='simpulse'):
+        if simulator is 'injectfrb':
+            data = self.gen_injfrb_pulse(ntime=10000, upchan_factor=1, 
+                                         upsamp_factor=1, conv_dmsmear=False)
+        elif simulator is 'simpulse':
+            data = self.gen_simpulse(ntime=10000)
+        else:
+            print("Expected either (injectfrb, simpulse)")
+            return 
+
+        freq_ref = 0.5*(self._freqs[0]+self._freqs[-1])
+
+        data_event_copy = data.copy()
+
+        data_event_copy = tools.dedisperse(data_event_copy, self._dm, self._dt, 
+                                           freq=(self._freqs[0],self._freqs[-1]), 
+                                           freq_ref=freq_ref)
+
+        mm = np.argmax(data_event_copy.mean(0))             
+        print(mm, data_event_copy.shape)
+        data_event_copy = data_event_copy[:, mm-500:mm+500]                                                                        
+        dmtarr, dms, times = tools.dm_transform(data_event_copy, self._freqs, dt=self._dt,                                                                           
+                                    freq_ref=freq_ref,                                                                       
+                                    dm_min=-50, dm_max=50, ndm=50)                                                                 
+        times += self._t0
+        dms += self._dm
+
+        dmtarr /= dmtarr.max()
+        dmtarr_function = interpolate.interp2d(times, dms, dmtarr)
+
+        return dmtarr, dms, times, dmtarr_function
+
+    def plot_dm_time_boxes(self, dm_arr, t_arr, t_err=0.5, width=None):
+        ntrig = len(dm_arr)
+        dm_err = 0.1*np.ones([ntrig])
+
+        if width is not None:
+            t_err += 0.1*width/0.001
+            dm_err = 0.1*dm_arr #30.18*1e3*width*(self._freqs.mean()/1400.)**-3
+            dm_low = dm_arr - dm_err
+            dm_high = dm_arr + dm_err
+        else:
+            t_err = np.ones([ntrig])*t_err
+            dm_low = ((1-dm_err)*dm_arr)
+            dm_high = ((1+dm_err)*dm_arr)            
+
+        fig = plt.figure()
+        plt.scatter(t_arr, dm_arr, s=5, facecolor='none', edgecolors='k')
+
+        for ii in range(ntrig):
+            times = np.linspace(t_arr[ii]-t_err[ii], t_arr[ii]+t_err[ii], 10)
+
+            plt.fill_between(times, dm_low[ii].repeat(10), dm_high[ii].repeat(10), alpha=0.4)
+
+
+    def find_parameter_guess(self, dm_arr, t_arr, snr_arr, 
+                             dm_err=0.5, t_err=1.):
+
+        dm_stat = np.abs(1.-dm_arr/self._dm)
+        t_stat = np.abs(self._t0 - t_arr)
+
+        ind = np.where((dm_stat<dm_err) & (t_stat<t_err))[0]
+
+        if len(ind)==0:
+            return [],[],[]
+
+        snr_max_ind = np.argmax(snr_arr[ind])
+
+        return dm_arr[ind[snr_max_ind]], t_arr[ind[snr_max_ind]], snr_arr[ind[snr_max_ind]]
+
+
+    def dm_time_contour_decision(self, dm_guess, t0_guess, thresh=0.1, 
+                                 simulator='simpulse', dmtarr_function='box'):
+
+        if type(dmtarr_function) is str:
+            if dmtarr_function=='bowtie':
+                dmtarr, dms, times, dmtarr_function = self.gen_dm_time_bowtie(simulator=simulator)
+            elif dmtarr_function=='gaussian':
+                dmtarr, dms, times, dmtarr_function = self.gen_dm_time_gaussian()
+            elif dmtarr_function=='box':
+                decision = self.dm_time_box_decision(dm_guess, t0_guess)
+                return decision
+
+        val = dmtarr_function(t0_guess, dm_guess)
+        decision = val > thresh
+
+        return decision[0]
+
+    def dm_time_box_decision(self, dm_guess, t0_guess, 
+                    dm_err=0.1, t_err=0.1):
         """ method to test if parameter 
         guess is within the allowed DM_time_box
 
@@ -154,86 +263,70 @@ class DetectionDecision():
         t_err : 
             allowed arrival time error in seconds 
         """
+        t_err += self._width_i / 0.001
 
-        dm_stat = np.abs(1.-dm_guess/self._dm)
+        dm_stat = np.abs(1.-np.float(dm_guess)/self._dm)
         t_stat = np.abs(self._t0 - t0_guess)
 
         decision = (dm_stat < dm_err) & (t_stat < t_err)
 
         return decision
 
-    def dm_time_ellipse(self):
+def get_decision_array(fn_truth, fn_cand, dmtarr_function='box'):
+    dm_truth, sig_truth, t_truth, w_truth = tools.read_singlepulse(fn_truth)
+    dm_cand, sig_cand, t_cand, w_cand = tools.read_singlepulse(fn_cand)
 
-        
-        freq_mid_GHz = 0.5*(self._freqs.max() + self._freqs.min())*1e-3
+    decision_arr = []
 
-        # FWHM of DM curve from Cordes & McLaughlin 2003
-        # although this equation's magnitude does not make sense... much too large.
-        dm_err = 506.*1e3*self._width_i * freq_mid_GHz**3 / self._delta_freq
-        print(dm_err)
+    for ii in range(len(dmt)):
+        D = DetectionDecision(dmt[ii], tt[ii])
+        dm_guess, t_guess, sig_guess = D.find_parameter_guess(dm_cand, 
+                                        t_cand, sig_cand, dm_err=0.5, t_err=1.)
 
-    def dm_time_contour(self, simulator='simpulse'):
-        if simulator is 'injectfrb':
-            data = self.gen_injfrb_pulse(ntime=10000, upchan_factor=1, upsamp_factor=1, conv_dmsmear=False)
-        elif simulator is 'simpulse':
-            data = self.gen_simpulse(ntime=10000)
-        else:
-            print("Expected either (injectfrb, simpulse)")
-            return 
+        if dm_guess==[]:
+            continue
 
-        data_event_copy = data.copy()
-        data_event_copy = tools.dedisperse(data_event_copy, self._dm, self._dt, 
-                                           freq=(self._freqs[0],self._freqs[-1]), 
-                                           freq_ref=None)
+        decision = int(D.dm_time_contour_decision(dm_guess, t_guess, 
+                                                  simulator='injectfrb', 
+                                                  dmtarr_function=dmtarr_function))
+        decision_arr.append(decision)
 
-        mm = np.argmax(data_event_copy.mean(0))                                                                                    
-        data_event_copy = data_event_copy[:, mm-250:mm+250]                                                                        
-        dmtarr = tools.dm_transform(data_event_copy, self._dt,                                                                           
-                                    self._freqs, ref_freq=None,                                                                       
-                                    dm_min=-50, dm_max=50, ndm=50)                                                                 
-                                                                                                                                   
-        indt = np.where(dmtarr.mean(0)>5e-3)[0]                                                                                    
-        plt.figure()                                                                                                               
-        plt.imshow(dmtarr[:, indt], aspect='auto') 
-        plt.show()
+    return decision_arr
 
 
-        return data, dmtarr
-
-    def plot_dm_time_boxes(self, dm_arr, t_arr, width=None):
-        t_err = 0.5
-        ntrig = len(dm_arr)
-        dm_err = 0.1*np.ones([ntrig])
-
-        if width is not None:
-            dm_err = 30.18*1e3*width*(self._freqs.mean()/1400.)**-3
-            dm_low = dm_arr - dm_err
-            dm_high = dm_arr + dm_err
-        else:
-            dm_low = ((1-dm_err)*dm_arr)
-            dm_high = ((1+dm_err)*dm_arr)            
-
-        fig = plt.figure()
-        plt.scatter(t_arr, dm_arr, s=5, facecolor='none', edgecolors='k')
-
-        for ii in range(ntrig):
-            print(ii, dm_err[ii])
-            times = np.linspace(t_arr[ii]-t_err, t_arr[ii]+t_err, 10)
-
-            plt.fill_between(times, dm_low[ii].repeat(10), dm_high[ii].repeat(10), alpha=0.4)
 
 
-    def find_parameter_guess(self, dm_arr, t_arr, snr_arr, 
-                             dm_err=0.5, t_err=1.):
-        print(self._dm)
-        print(self._t0)
-        dm_stat = np.abs(1.-dm_arr/self._dm)
-        t_stat = np.abs(self._t0 - t_arr)
 
-        ind = np.where((dm_stat<dm_err) & (t_stat<t_err))[0]
-        snr_max_ind = np.argmax(snr_arr[ind])
 
-        return snr_max_ind
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
