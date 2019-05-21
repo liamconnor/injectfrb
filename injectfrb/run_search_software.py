5825e4ddad62614f46b22b5e5391a6b2bd17707e
Liam Connor <liam.dean.connor@gmail.com>
2019-05-21 12:34:15 +0200

    script to run all search soft

--- injectfrb/run_search_software.py
+++ injectfrb/run_search_software.py
@@ -10,7 +10,8 @@ outdir = './'
 fnout = 'output'
 
 heim_args = (fnfil, dm_min, dm_max, outdir)
-heimdall_str = 'heimdall -v -f %s -dm %f %f -rfi_no_narrow -rfi_no_broad -output_dir %s' % heim_args
+heimdall_str = 'heimdall -v -f %s -dm %f %f -rfi_no_narrow -rfi_no_broad -output_dir /tmp/' % heim_args
+heimdall_post_str = 'cat /tmp/*.cand > %s/%s.cand' % (outdir, outfn)
 
 amber_str = 'python run_amber_args.py %s' % fnfil
 
@@ -18,6 +19,7 @@ fredda_str = 'cudafdmt -t 512 -d 16384 -x 6 -o %s/%s.fredda %s' % (outdir, fnout
 
 print("Starting Heimdall")
 os.system(heimdall_str)
+os.system(heimdall_post_str)
 print("Starting Amber")
 os.system(amber_str)
 print("Starting Fredda")
