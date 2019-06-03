16537725a227ad994e04052910447e3deec542db
Liam Connor <liam.dean.connor@gmail.com>
2019-06-03 09:54:40 +0200

    fixing setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,28 @@
-from setuptools import setup
+from setuptools import setup, Extension
+import numpy as np
 
-setup(name='injectfrb',
-      version='0.1',
-      description='Software for simulating fast radio bursts ',
-      url='http://github.com/liamconnor/injectfrb',
-      author='Flying Circus',
-      author_email='liam.dean.connor@gmail.com',
-      license='MIT',
-      packages=['injectfrb'],
-      zip_safe=False)
+VERSION_MAJOR = 0
+VERSION_MINOR = 1
+VERSION_POINT = 0
+VERSION_DEV = 1
+
+VERSION = "%d.%d.%d" % (VERSION_MAJOR, VERSION_MINOR, VERSION_POINT)
+if VERSION_DEV:
+    VERSION = VERSION + ".dev%d" % VERSION_DEV
+
+SCRIPTS = ["injectfrb/inject_gaussian_fil.py",
+		   ]
+
+setup(
+    name = "injectfrb",
+    version = VERSION,
+    packages = ["injectfrb"],
+    scripts = SCRIPTS,
+    install_requires = ['numpy', 'matplotlib'],
+    # metadata for upload to PyPI
+    author = "Liam Connor",
+    author_email = "liam.dean.connor@gmail.com",
+    description = "Software for simulating fast radio bursts",
+    license = "GPL v2.0",
+    url="http://github.com/liamconnor/injectfrb",
+)
